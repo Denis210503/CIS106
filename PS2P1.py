2f382930e4b4aaec1b2d1e794e6cbb00694373a5
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-02-28 19:16:20 -0600

    Update PS2P1.py

--- PS2P1.py
+++ PS2P1.py
@@ -1,4 +1,4 @@
 amount = input("Enter quantity you want to purchase\n")
 price = input("enter the cost per times\n")
-cost = int(amount) * int(price)
+cost = float(amount) * float(price)
 print("The total cost is", cost)
