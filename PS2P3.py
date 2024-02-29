f678a6a01643fe6ea8f82cf84aeb613f23c279d0
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-02-28 19:36:54 -0600

    Create PS2P3.py

---
+++ PS2P3.py
@@ -0,0 +1,7 @@
+width = input("what is the width of the rectangle?\n")
+length = input("what is the length of the rectangle?\n")
+area = float(length) * float(width)
+print("The area of the rectangle is", area)
+circumefrence = float(width) * 2 + float(length) * 2
+
+print("the circumeference of the rectangle is", circumefrence)
