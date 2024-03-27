3ae34dfe7d43affc708c3ba2a21a5e772f4873dd
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:30:35 -0500

    Update PS3P3.py

--- PS3P3.py
+++ PS3P3.py
@@ -1,1 +1,5 @@
+amount_earned = float(input("Enter the amount earned: "))
+amount_earned = amount_earned / 3
+amount_earned = round(amount_earned, 2)
+print("Each member will receive ", amount_earned, "dollars")
 
