590c84b93e6c0d7496a10f7e70bb8c5d9f09a94c
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:33:06 -0500

    Update PS5P4.py

--- PS5P4.py
+++ PS5P4.py
@@ -1,1 +1,14 @@
+appliance_name = input("Enter the name of the appliance: ")
+appliance_cost = float(input("Enter the cost of the appliance: "))
 
+if appliance_cost > 1000:
+  warranty_cost = appliance_cost * 0.1
+else:
+  warranty_cost = appliance_cost * 0.05
+
+total = appliance_cost + warranty_cost
+
+print("the name of the appliance is", appliance_name)
+print("the cost of the appliance is", appliance_cost)
+print("the cost of the warranty is", warranty_cost)
+print("the total cost is", total)
