422819d69a470e603715dd9d278bfe9237ff5d59
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:31:43 -0500

    Update PS4P2.py

--- PS4P2.py
+++ PS4P2.py
@@ -1,1 +1,12 @@
+price_per_share = float(
+    input("Price per share at the moment of transaction : "))
+current_stock_price = float(input("Enter the current stock price: "))
+quantity_of_stock = float(input("Enter the quantity of stock: "))
+total = (current_stock_price - price_per_share) * quantity_of_stock
+if total > 0:
+  print("After the transaction, you gained $", total)
+elif total < 0:
+  print("After the transaction, you lost $", total)
+else:
+  print("After the transaction, you broke even")
 
