64616d4b972afa0a89eb6ceddf854c975892a400
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:33:57 -0500

    Update PS6P2.py

--- PS6P2.py
+++ PS6P2.py
@@ -1,1 +1,17 @@
+part_number = input("Enter the part number: ")
+quantity = int(input("Enter the quantity: "))
 
+if part_number == "10" or part_number == "55":
+    unit_cost = 1.00
+elif part_number == "99":
+    unit_cost = 2.00
+elif part_number == "80" or part_number == "70":
+    unit_cost = 3.00
+else:
+    unit_cost = 5.00
+
+total_cost = quantity * unit_cost
+
+print("Part number:", part_number)
+print("cost per unit: $", unit_cost)
+print("total cost: $", total_cost)
