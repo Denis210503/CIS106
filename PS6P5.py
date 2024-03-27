756a1b004c4261502c3d62545aee9ff28b933ec1
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:34:40 -0500

    Update PS6P5.py

--- PS6P5.py
+++ PS6P5.py
@@ -1,1 +1,15 @@
+last_name = input("Enter employee's last name: ")
+salary = float(input("Enter employee's salary: "))
+job_level = int(input("Enter employee's job level: "))
 
+if job_level >= 10:
+    bonus_rate = 0.25
+elif 5 <= job_level <= 9:
+    bonus_rate = 0.20
+else:
+    bonus_rate = 0.10
+
+bonus = salary * bonus_rate
+
+print("employee's last name:", last_name)
+print("bonus: $", bonus)
