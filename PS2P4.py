3cdd6829fb996f4b30408a85c007c9cc0fe27cbb
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:29:12 -0500

    Update PS2P4.py

--- PS2P4.py
+++ PS2P4.py
@@ -1,1 +1,6 @@
-# TO DO
+name = input("What is your name? ")
+credits_taken = int(input("how many credits have you taken ? "))
+tuition = 250
+lab_fee = 100
+total_tuition = credits_taken * tuition + lab_fee
+print(name + " your total tuition is ", total_tuition)
