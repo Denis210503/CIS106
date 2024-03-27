41332e9b771f7e471ba53c0b5922b6da9894cf1c
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:32:13 -0500

    Update PS4P4.py

--- PS4P4.py
+++ PS4P4.py
@@ -1,1 +1,5 @@
+name = input("Enter your name: ")
+steps_walked = int(input("Enter the number of steps walked: "))
+calories_burned = steps_walked * 0.25
+print(name, "you burned", calories_burned, "calories")
 
