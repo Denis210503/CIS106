c1cbb38a5163c594465efe62c36a02548b962ee8
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:31:17 -0500

    Update PS3P5.py

--- PS3P5.py
+++ PS3P5.py
@@ -1,1 +1,6 @@
+radius_of_circle = float(input("Enter the radius of the circle: "))
+area = 3.174 * radius_of_circle**2
+perimeter = 2 * 3.174 * radius_of_circle
+print("the area of the circle is", area)
+print("the perimeter of the circle is", perimeter)
 
