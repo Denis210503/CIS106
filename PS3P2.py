de423469db690408627f895b783175aa6711f662
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:30:02 -0500

    Update PS3P2.py

--- PS3P2.py
+++ PS3P2.py
@@ -1,1 +1,6 @@
+last_name = input("Enter your last name: ")
+midterm_grade = float(input("Enter your midterm grade: "))
+final_exam_grade = float(input("Enter your final exam grade: "))
+total_exam_points = midterm_grade + final_exam_grade
+print(last_name + " your total exam points are ", total_exam_points)
 
