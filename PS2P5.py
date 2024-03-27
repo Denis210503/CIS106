76025baa5f020e5ff99e451b62537cbd49075e35
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:29:30 -0500

    Update PS2P5.py

--- PS2P5.py
+++ PS2P5.py
@@ -1,1 +1,7 @@
-# TO DO
+item_price = float(input("Enter the price of the item: "))
+item_discountpercent = float(input("Enter the discount percentage: % "))
+new_price = item_price - item_price * item_discountpercent / 100
+discount_price = item_price - new_price
+print("the item has had a discount of ", discount_price)
+print("The new price of the item is: ", new_price)
+
