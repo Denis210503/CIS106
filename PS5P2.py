ffb4bbfc52e57ca28d540492cc1fe55b67c7cb1c
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:32:47 -0500

    Update PS5P2.py

--- PS5P2.py
+++ PS5P2.py
@@ -1,1 +1,14 @@
+item = input("Enter the item: (A OR B) : ")
+quantity = int(input("enter a quantity : "))
+unit_price = 0
 
+if item == "A":
+  unit_price = 10
+else:
+  unit_price = 20
+
+extended_price = unit_price * quantity
+
+print("item :", item)
+print("unit price :", unit_price)
+print("extended price :", extended_price)
