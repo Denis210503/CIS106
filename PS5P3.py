c7a203e364283c38fb7fa3a946ba9f5fff02c395
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:32:57 -0500

    Update PS5P3.py

--- PS5P3.py
+++ PS5P3.py
@@ -1,1 +1,12 @@
+number_of_books = int(input("Enter the number of books: "))
+cost_per_book = int(input("Enter the cost per book: "))
+total = number_of_books * cost_per_book
+if total > 50:
+  shipping_cost = 0
+ 
+else:
+  shipping_cost = 25
+
+print("the total for the the order is", total ,"$")
+print("the shipping and handling is", shipping_cost, "$")
 
