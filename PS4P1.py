dbd813d3536312f03c91c512b87bdf25c2fc5a26
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:31:29 -0500

    Update PS4P1.py

--- PS4P1.py
+++ PS4P1.py
@@ -1,1 +1,7 @@
+exam_score1 = float(input("Enter the first exam score: "))
+exam_score2 = float(input("Enter the second exam score: "))
 
+exam_score1 = exam_score1 * 0.6
+exam_score2 = exam_score2 * 0.4
+total_score = exam_score1 + exam_score2
+print("The total score is: ", total_score)
