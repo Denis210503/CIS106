9a03eb960397707236c19be85daf43a3160af2ce
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:30:51 -0500

    Update PS3P4.py

--- PS3P4.py
+++ PS3P4.py
@@ -1,1 +1,15 @@
+make = input("Enter the brand of the car:")
+model = input("Enter the model of the car:")
+msrp_amount = float(input("Enter the MSRP amount of the car:"))
+discount_percent1 = float(input("Enter the discount percent:"))
+discount_percent2 = discount_percent1 / 100
+discount_amount = msrp_amount * discount_percent2
+discount_amount = round(discount_amount, 2)
+final_price = msrp_amount - discount_amount
+final_price = round(final_price, 2)
+
+print("the original price for the", make, model, "is", msrp_amount, "dollars")
+print("with a discount of %", discount_percent1,
+      "the amount taken off the final price is", discount_amount)
+print("the final discounted price is", final_price, "dollars")
 
