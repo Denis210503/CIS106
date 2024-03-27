68695ec80d173ffccf06fa1cbdb2051e89d58895
Denis210503 <160086849+Denis210503@users.noreply.github.com>
2024-03-27 15:34:05 -0500

    Update PS6P3.py

--- PS6P3.py
+++ PS6P3.py
@@ -1,1 +1,21 @@
+principle = float(input("Enter the principle amount of the CD: "))
+years_to_maturity = int(input("Enter the years to maturity of the CD: "))
+
+if principle > 100000:
+    interest_rate = 0.06
+elif 50000 <= principle <= 100000:
+    if years_to_maturity == 10:
+        interest_rate = 0.05
+    elif years_to_maturity == 5:
+        interest_rate = 0.04
+    else:
+        interest_rate = 0.02
+else:
+    interest_rate = 0.02
+
+first_year_interest = principle * interest_rate
+
+print("principle: $", principle)
+print("interest rate:", interest_rate)
+print("interest amount for the first yeat: $", first_year_interest)
 
